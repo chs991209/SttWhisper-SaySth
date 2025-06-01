d5b9cc77aedb4c00c6689b1e181ab148882f1b18
이태훈 <powerlth@jbnu.ac.kr>
2025-06-02 00:47:03 +0900

    코드 리펙토링, 파일 구조화

--- app/main.py
+++ app/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from .config import lifespan
 from .middleware import setup_middleware
 from fastapi.staticfiles import StaticFiles
-from .routes import stt_base64
+from app.routes import routers
 #from .routes import index
 
 app = FastAPI(lifespan=lifespan)
 
 setup_middleware(app)
 app.mount("/static", StaticFiles(directory="static"), name="static")
-app.include_router(stt_base64.router)
+for router in routers:
+    app.include_router(router)
 #app.include_router(index.router)
